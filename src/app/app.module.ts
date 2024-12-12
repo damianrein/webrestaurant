7b6b8f2af39d5b4cf5ffc800b30f9518ff8153a2
damianrein <damianrein464@gmail.com>
2024-12-11 21:52:47 -0300

    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ApiComponent } from './services/api/api.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ApiComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
